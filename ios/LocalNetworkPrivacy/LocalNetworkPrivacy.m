f69567368b265897c3749e63d42840553456f034
Jegor <j.alexeichuck@hqsoftwarelab.com>
2024-10-17 16:22:43 +0200

    Fix local network permission dialogue appearance [APP-211] [PLAN-530]
    - Trigger iOS system dialog when user requests for  local permission for the first time
    - Ignore `LocalNetworkPrivacy.checkAccessState` result for the first time
    - Remove unnecessary logic from `LocalNetworkPrivacy` file
    
    # Related
     - https://planitar.atlassian.net/browse/PLAN-530
     - https://planitar.atlassian.net/browse/APP-190

--- ios/LocalNetworkPrivacy/LocalNetworkPrivacy.m
+++ ios/LocalNetworkPrivacy/LocalNetworkPrivacy.m
@@ -12,8 +12,6 @@
 
 @implementation LocalNetworkPrivacy
 
-static NSString * const kLocalNetworkPermissionRequestedKey = @"LocalNetworkPermissionRequested";
-
 - (instancetype)init {
     if (self = [super init]) {
         self.service = [[NSNetService alloc] initWithDomain:@"local." type:@"_lnp._tcp." name:@"LocalNetworkPrivacy" port:1100];
@@ -28,50 +26,21 @@ static NSString * const kLocalNetworkPermissionRequestedKey = @"LocalNetworkPerm
 - (void)checkAccessState:(void (^)(BOOL))completion {
     self.completion = completion;
 
-    BOOL permissionRequestedBefore = [[NSUserDefaults standardUserDefaults] boolForKey:kLocalNetworkPermissionRequestedKey];
+    self.publishing = YES;
+    self.service.delegate = self;
+    [self.service publish];
 
-    if (!permissionRequestedBefore) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kLocalNetworkPermissionRequestedKey];
-
-        self.publishing = YES;
-        self.service.delegate = self;
-        [self.service publish];
-
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:0.5 repeats:YES block:^(NSTimer * _Nonnull timer) {
-            if (!self.publishing) {
-                [self completeWithResult:self.service.includesPeerToPeer];
-            }
-        }];
-    } else {
-        self.publishing = YES;
-        self.service.delegate = self;
-        [self.service publish];
-
-        self.timer = [NSTimer scheduledTimerWithTimeInterval:2 repeats:NO block:^(NSTimer * _Nonnull timer) {
-            [self completeWithResult:NO];
-        }];
-    }
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:1 repeats:NO block:^(NSTimer * _Nonnull timer) {
+        [self.timer invalidate];
+        self.completion(NO);
+    }];
 }
 
 #pragma mark - NSNetServiceDelegate
 
 - (void)netServiceDidPublish:(NSNetService *)sender {
-    self.publishing = NO;
-    [self completeWithResult:YES];
-}
-
-- (void)netService:(NSNetService *)sender didNotPublish:(NSDictionary<NSString *,NSNumber *> *)errorDict {
-    self.publishing = NO;
-    [self completeWithResult:NO];
-}
-
-#pragma mark - Private Methods
-
-- (void)completeWithResult:(BOOL)result {
     [self.timer invalidate];
-    if (self.completion) {
-        self.completion(result);
-    }
+    self.completion(YES);
 }
 
 @end
